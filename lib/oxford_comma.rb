1cc40cdf32a1f5938cc7dfc91b37f9f77512b4a2
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-10 12:30:14 -0500

    commit tree ed52c8bccac96098dac8a879f9dea1234f539f53

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array = array.join
+        array
+    elsif array.length == 2
+        new_str = array.join(" and ")
+        new_str
+    elsif array.length > 2
+        new_str = array.join(", ")
+        position = new_str.rindex(", ")
+        new_str = new_str.insert(position + 2, "and ")  
+        new_str
+    end
 end
+
+example_array = ["spinach"]
+oxford_comma(example_array)
+
+example_array = ["spinach", "potatoes with hats"]
+oxford_comma(example_array)
+
+example_array = ["spinach", "potatoes with hats", "earwigs"]
+oxford_comma(example_array)
+
+example_array = ["spinach", "potatoes with hats", "earwigs", "bungalows", "osteopaths"]
+oxford_comma(example_array)
